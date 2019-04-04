b5d94db78e68ab929b6b30535db20f0eb676689a
Victor San Kho Lin <victorsankho.lin@unimelb.edu.au>
2019-04-04 18:28:31 +1100

    Fix with openstack role add command

--- add-user.pl
+++ add-user.pl
@@ -13,6 +13,8 @@ while (my $line = <$data>) {
 	my $projectId = $fields[3];
 	my $userName = $fields[5];
 
+    $ENV{'OS_PROJECT_NAME'} = $projectName;
+
 	chomp $userName;	
 	if($userName eq "") {
 		print "Skipping - Empty Username \n";
@@ -20,10 +22,8 @@ while (my $line = <$data>) {
 	else {
 		print "Attempting to add $userName to $projectName ...\n";
 		my $userEmail = $userName . '@student.unimelb.edu.au';
-		#print "openstack --username $userEmail --project $projectId Memeber \n";
-		my $output = `openstack --username $userEmail --project $projectId Member`;
+		#print "openstack role add --user $userEmail --project $projectId Memeber \n";
+		my $output = `openstack role add --user $userEmail --project $projectId Member`;
 		print "Output: $output \n"
 	}
-
 }
-
